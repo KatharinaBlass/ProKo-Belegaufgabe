18e16587c8804350b3b4e4709574311479558adb
Kathi <katharina.blass@student.hpi.de>
2020-10-20 15:44:26 +0200

    setup openMP

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <omp.h>
+
 using namespace cv;
 
 int main(int argc, char** argv )
 {
-    std::cout << "hello world!";
-    if ( argc != 2 )
+    // std::cout << "hello world!";
+    /*if ( argc != 2 )
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
@@ -26,7 +28,12 @@ int main(int argc, char** argv )
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
 
-    waitKey(0);
+    waitKey(0);*/
+
+    #pragma omp parallel for
+    for (int i=0; i<4; i++) {
+        printf("hello aus dem theead %d von %d\n", omp_get_thread_num(), omp_get_num_threads());
+    }
 
     return 0;
 }
